6d2998d63a559c3a3345e851d9e06154cbf4905a
JUNGYUMI <dbal7749@naver.com>
2020-05-14 15:04:13 +0900

    16일차 - 아이디 만들기

---
+++ src/day16/MapEx1.java
@@ -0,0 +1,25 @@
+package day16;
+
+import java.util.*;
+
+public class MapEx1 {
+
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("abcde", "1234");
+		map.put("abcde", "12345");
+		map.put("abcdef", "1234");
+	
+		Set set = map.entrySet();
+		Iterator it = set.iterator();
+		while(it.hasNext()) {
+			Map.Entry e = (Map.Entry)it.next();
+			System.out.println("id : " + e.getKey());
+			System.out.println("pw : " + e.getValue());
+			System.out.println("======================");
+		}
+		Set keySet = map.keySet();					// 중복되지 않아서 Set
+		Collection valueSet = map.values();		
+	}
+}
+

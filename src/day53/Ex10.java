eebe6e26425e1b2e90b8acdcc34c19fcbdb30af8
JUNGYUMI <dbal7749@naver.com>
2020-07-06 17:08:07 +0900

    53일차 - 오버로딩

---
+++ src/day53/Ex10.java
@@ -0,0 +1,18 @@
+package day53;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		// 
+	}
+	public static double sum(double num1, double num2) {
+		return num1 + num2;
+	}
+	public static double sum(double num1, double num2, double num3) {
+		return sum(num1, num2) + num3;
+	}
+	// 에러 발생 : 오버로딩 가능 여뷰에서 리턴타입은 중요하지 않다.
+	/* public static int sum(double num1, double num2) {
+		return 0; 
+	} */
+}

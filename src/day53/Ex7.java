a7763e3b8a8a6de989455b22485fdbdcb79d6786
JUNGYUMI <dbal7749@naver.com>
2020-07-07 17:05:13 +0900

    53일차 - 수정

--- src/day53/Ex7.java
+++ src/day53/Ex7.java
@@ -6,8 +6,8 @@ public class Ex7 {
 		// 1부터 10까지의 짝수의 합을 continue를 이용하여 작성하세요.
 		int sum = 0;
 		for(int i = 1; i<=10; i++) {
-			if(i%2==1)
-				continue;
+			if(i%2==1) //홀수 
+				continue; // 홀수는 합을 구하지 않음
 			sum+=i;
 		}
 		System.out.println("1부터 10까지의 합 : " + sum);
